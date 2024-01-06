c3cf9bc81a13aed3eec8d613e19d555fd82b65db
Ziggy <zigzter@users.noreply.github.com>
2024-01-05 23:34:28 -0800

    LSP fixes

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,62 +1,74 @@
 return {
-    'neovim/nvim-lspconfig',
-    dependencies = {
-        'hrsh7th/cmp-nvim-lsp',
-    },
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-        local lspconfig = require('lspconfig')
-        local cmp_lsp = require('cmp_nvim_lsp')
-        local map = vim.keymap
-        local opts = { noremap = true, silent = true }
-        local on_attach = function(client, bufnr)
-            opts.buffer = bufnr
-            opts.desc = 'Show LSP references'
-            map.set('n', 'gR', '<cmd>Telescope lsp_references<CR>', opts)
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local lspconfig = require("lspconfig")
+		local cmp_lsp = require("cmp_nvim_lsp")
+		local map = vim.keymap
+		local opts = { noremap = true, silent = true }
+		local on_attach = function(client, bufnr)
+			opts.buffer = bufnr
+			opts.desc = "Show LSP references"
+			map.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
 
-            opts.desc = '[g]o to [d]ecleration'
-            map.set('n', 'gd', vim.lsp.buf.declaration, opts)
+			opts.desc = "[g]o to [d]ecleration"
+			map.set("n", "gd", vim.lsp.buf.declaration, opts)
 
-            opts.desc = 'Show line diagnostics'
-            map.set('n', '<leader>d', vim.diagnostic.open_float, opts)
+			opts.desc = "Show line diagnostics"
+			map.set("n", "<leader>d", vim.diagnostic.open_float, opts)
 
-            opts.desc = 'Show documentation of what is under cursor'
-            map.set('n', 'K', vim.lsp.buf.hover, opts)
+			opts.desc = "Show buffer diagnostics"
+			map.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
 
-            opts.desc = 'Go to previous diagnostic line'
-            map.set('n', '[d', vim.diagnostic.goto_prev, opts)
+			opts.desc = "Show all buffer diagnostics"
+			map.set("n", "<leader>aD", "<cmd>Telescope diagnostics<CR>", opts)
 
-            opts.desc = 'Go to next diagnostic line'
-            map.set('n', ']d', vim.diagnostic.goto_next, opts)
+			opts.desc = "Show documentation of what is under cursor"
+			map.set("n", "K", vim.lsp.buf.hover, opts)
 
-            opts.desc = 'Restart LSP'
-            map.set('n', '<leader>rs', ':LspRestart<CR>', opts)
-        end
-        local capabilities = cmp_lsp.default_capabilities()
+			opts.desc = "Smart rename"
+			map.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 
-        local signs = { Error = ' ', Warn = ' ', Hint = '󰌵 ', Info = '󰋼 ' }
-        for type, icon in pairs(signs) do
-            local hl = "DiagnosticSign" .. type
-            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
-        end
+			opts.desc = "Go to previous diagnostic line"
+			map.set("n", "[d", vim.diagnostic.goto_prev, opts)
 
-        lspconfig.dockerls.setup{}
-        lspconfig.tsserver.setup{}
-        lspconfig.eslint.setup{}
-        lspconfig.gopls.setup{}
-        lspconfig.html.setup{}
-        lspconfig.jsonls.setup{}
-        lspconfig.lua_ls.setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
-            settings = {
-                Lua = {
-                    diagnostics = {
-                        globals = { 'vim' },
-                    },
-                },
-            },
-        })
-        lspconfig.rubocop.setup{}
-    end
+			opts.desc = "Go to next diagnostic line"
+			map.set("n", "]d", vim.diagnostic.goto_next, opts)
+
+			opts.desc = "Restart LSP"
+			map.set("n", "<leader>rs", ":LspRestart<CR>", opts)
+		end
+		local capabilities = cmp_lsp.default_capabilities()
+
+		local signs = { Error = " ", Warn = " ", Hint = "󰌵 ", Info = "󰋼 " }
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		end
+
+		lspconfig.dockerls.setup({})
+		lspconfig.tsserver.setup({})
+		lspconfig.eslint.setup({})
+		lspconfig.gopls.setup({
+			cmd = { "gopls", "--remote=auto" },
+			on_attach = on_attach,
+		})
+		lspconfig.html.setup({})
+		lspconfig.jsonls.setup({})
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
+					},
+				},
+			},
+		})
+		lspconfig.rubocop.setup({})
+	end,
 }
