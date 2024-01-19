78292f1dcf92da5026756ab3bc7996c7950a9acd
Ziggy <zigs_17@hotmail.com>
2024-01-18 20:06:51 -0800

    Lualine updates, LSP shortcut updates

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,12 +1,34 @@
 return {
-	'nvim-lualine/lualine.nvim',
-	dependencies = { 'nvim-tree/nvim-web-devicons', opt = true },
-	opts = {
-		options = {
-			theme = "catppuccin",
-		},
-		sections = {
-			lualine_c = { { 'filename', path = 1 } },
-		}
-	}
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons', opt = true },
+    opts = {
+        options = {
+            theme = "catppuccin",
+            component_separators = '|',
+            section_separators = { left = '', right = '' },
+            globalstatus = true
+        },
+        sections = {
+            lualine_a = {
+                { 'mode', separator = { left = '' }, right_padding = 2 },
+            },
+            lualine_b = { 'filename', 'branch' },
+            lualine_c = {},
+            lualine_x = {},
+            lualine_y = { 'filetype' },
+            lualine_z = {
+                { 'location', separator = { right = '' }, left_padding = 2 },
+            },
+        },
+        inactive_sections = {
+            lualine_a = { 'filename' },
+            lualine_b = {},
+            lualine_c = {},
+            lualine_x = {},
+            lualine_y = {},
+            lualine_z = { 'location' },
+        },
+        tabline = {},
+        extensions = {},
+    }
 }
