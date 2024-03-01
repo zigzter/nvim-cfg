fd9e76c2b4b0f229f6818c397f65c3216d55b249
Ziggy <zigzter@users.noreply.github.com>
2024-03-01 11:12:25 -0800

    Replace leap & quickscope with flash
    Flash handles both cases of leap and quickscope, plus the treesitter
    integration is something I could really use.

--- lua/plugins/quickscope.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "unblevable/quick-scope",
-    init = function()
-        vim.cmd [[
-        highlight QuickScopePrimary guifg='#ea76cb' gui=underline ctermfg=155 cterm=underline
-        highlight QuickScopeSecondary guifg='#d20f39' gui=underline ctermfg=81 cterm=underline
-        ]]
-    end
-}
